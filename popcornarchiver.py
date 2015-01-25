f9eb5b86023d1c6651edc5aab09ac800b398fe00
SirNeon618 <SirNeon618@users.noreply.github.com>
2015-01-25 12:49:38 -0500

    Update popcornarchiver.py

--- popcornarchiver.py
+++ popcornarchiver.py
@@ -15,10 +15,10 @@ from time import sleep
 config = simpleconfigparser()
 
 if not(os.path.isfile("settings.cfg")):
-print("Couldn't find settings.cfg. Exiting.")
-exit(1)
+    print("Couldn't find settings.cfg. Exiting.")
+    exit(1)
 else:
-config.read("settings.cfg")
+    config.read("settings.cfg")
 
 con = db.connect("alreadyarchived.db")
 cur = con.cursor()
@@ -36,126 +36,108 @@ SUBREDDIT_LIST = list(set(str(config.main.subreddit_list).split(',')))
 
 
 def login(username, password):
-print("Logging in as {}...".format(username))
+    print("Logging in as {}...".format(username))
 
-while True:
-    try:
-        client.login(username, password)
-        break
-    except (HTTPError, timeout) as e:
-        print(e)
-        continue
-    except (InvalidUser, InvalidUserPass, RateLimitExceeded, APIException) as e:
-        print(e)
-        exit(1)
+    while True:
+        try:
+            client.login(username, password)
+            break
+        except (HTTPError, timeout) as e:
+            print(e)
+            continue
+        except (InvalidUser, InvalidUserPass, RateLimitExceeded, APIException) as e:
+            print(e)
+            exit(1)
 
 
 def get_submissions(multireddit):
-while True:
-    try:
-        submissions = client.get_subreddit(multireddit).get_new(limit=SUBMISSION_LIMIT)
-        break
-    except (HTTPError, timeout) as e:
-        continue
+    while True:
+        try:
+            submissions = client.get_subreddit(multireddit).get_new(limit=SUBMISSION_LIMIT)
+            break
+        except (HTTPError, timeout) as e:
+            continue
 
-return submissions
+    return submissions
 
 
 def get_reddit_urls(submission):
-# matches reddit submissions, but not just
-# https://www.reddit.com/r/SomeSubreddit
-reddit_url_pattern = re.compile("https?\:\/\/[\w.]+reddit\.com\/r\/[\w\d]+\/[\w\d/]+")
-
-if(submission.is_self):
-    reddit_urls = reddit_url_pattern.findall(str(submission.selftext))
-    if(reddit_urls != []):
-        return reddit_urls
+    # matches reddit submissions, but not just
+    # https://www.reddit.com/r/SomeSubreddit
+    reddit_url_pattern = re.compile("https?\:\/\/[\w.]+reddit\.com\/r\/[\w\d]+\/[\w\d/]+")
+    
+    if(submission.is_self):
+        reddit_urls = reddit_url_pattern.findall(str(submission.selftext))
+        if(reddit_urls != []):
+            return reddit_urls
+        else:
+            return None
     else:
-        return None
-else:
-    reddit_url = reddit_url_pattern.match(str(submission.url))
-    if reddit_url is not None:
-        return [reddit_url.group(0)]
-    else:
-        return None
+        reddit_url = reddit_url_pattern.match(str(submission.url))
+        if reddit_url is not None:
+            return [reddit_url.group(0)]
+        else:
+            return None
 
 
 def get_archive_today(link):
-while True:
-    try:
-        archive_submitter.open("http://archive.today")
-        archive_submitter.select_form(nr=0)
-        archive_submitter["url"] = link
-        archive_submitter.submit()
-        
-        # this is necessary to get the actual archive url instead of
-        # http://archive.today/submit
-        response = archive_submitter.reload()
-        break
-    except Exception:
-        continue
+    while True:
+        try:
+            archive_submitter.open("http://archive.today")
+            archive_submitter.select_form(nr=0)
+            archive_submitter["url"] = link
+            archive_submitter.submit()
+            
+            # this is necessary to get the actual archive url instead of
+            # http://archive.today/submit
+            response = archive_submitter.reload()
+            break
+        except Exception:
+            continue
 
-return response.geturl()
+    return response.geturl()
 
 
 def format_comment(source_archive_dict):
-comment_body = ""
+    comment_body = ""
 
-for source, archive in source_archive_dict.iteritems():
-    comment_body += "[[source]]({}) ".format(source)
-    comment_body += "[[archive.today]]({})\n\n".format(archive[0])
+    for source, archive in source_archive_dict.iteritems():
+        comment_body += "[[source]]({}) ".format(source)
+        comment_body += "[[archive.today]]({})\n\n".format(archive[0])
+    
+    comment_body += "\n---\n[Open Source]({}) |".format("https://github.com/SirNeon618/PopcornArchiver")
+    comment_body += " [Feedback]({})\n".format("https://www.reddit.com/message/compose?to=%2Fr%2FPopcornArchiver")
 
-comment_body += "\n---\n[Open Source]({}) |".format("https://github.com/SirNeon618/PopcornArchiver")
-comment_body += " [Feedback]({})\n".format("https://www.reddit.com/message/compose?to=%2Fr%2FPopcornArchiver")
-
-return comment_body
+    return comment_body
 
 
 def submit_comment(submission, text):
-while True:
-    try:
-        submission.add_comment(text)
-        break
-    except (HTTPError, timeout) as e:
-        continue
+    while True:
+        try:
+            submission.add_comment(text)
+            break
+        except (HTTPError, timeout) as e:
+            continue
 
 
 def main():
-login(USERNAME, PASSWORD)
-multireddit = ""
+    login(USERNAME, PASSWORD)
+    multireddit = ""
 
-for subreddit in SUBREDDIT_LIST:
-    multireddit += subreddit + '+'
+    for subreddit in SUBREDDIT_LIST:
+        multireddit += subreddit + '+'
 
-multireddit = multireddit.strip('+')
+    multireddit = multireddit.strip('+')
 
-while True:
-    submissions = get_submissions(multireddit)
+    while True:
+        submissions = get_submissions(multireddit)
 
-    for submission in submissions:
-        permalink = str(submission.permalink)
-        cur.execute("SELECT permalink FROM submissions WHERE permalink=?", (permalink,))
-        
-        if(cur.fetchone() != None):
-            continue
+        for submission in submissions:
+            permalink = str(submission.permalink)
+            cur.execute("SELECT permalink FROM submissions WHERE permalink=?", (permalink,))
+            
+            if(cur.fetchone() != None):
+                continue
 
-        archived_links = {}
-        links = get_reddit_urls(submission)
-
-        if links is not None:
-            for link in links:
-                archive_today = get_archive_today(link)
-                # uses a list in case multiple archive services are desireable.
-                archived_links[link] = [archive_today]
-
-            text = format_comment(archived_links)
-            submit_comment(submission, text)
-
-        cur.execute("INSERT INTO submissions VALUES(?)", (permalink,))
-        con.commit()
-
-        sleep(60)
-
-
-if __name__ == "__main__":
-main()
+            archived_links = {}
+            links = get_reddit_urls(submission)
