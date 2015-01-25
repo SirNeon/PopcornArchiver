049c25b0595546b43bdf2d49c28d3e8a51f74fbc
SirNeon618 <SirNeon618@users.noreply.github.com>
2015-01-25 10:38:31 -0500

    Update popcornarchiver.py

--- popcornarchiver.py
+++ popcornarchiver.py
@@ -1,231 +1,161 @@
 from __future__ import print_function
-from os.path import isfile
-import re
-from socket import error, timeout
-import sqlite3 as db
-from mechanize import Browser
+import mechanize
+import os
 import praw
 from praw.errors import *
+import re
 from requests.exceptions import HTTPError
+from socket import *
+import sqlite3 as db
 from simpleconfigparser import simpleconfigparser
+from sys import exit
+from time import sleep
 
 
-class SettingsError(Exception):
-    """
-    Gets raised when the settings.cfg file is missing.
-    """
+config = simpleconfigparser()
+
+if not(os.path.isfile("settings.cfg")):
+    print("Couldn't find settings.cfg. Exiting.")
+    exit(1)
+else:
+    config.read("settings.cfg")
+
+con = db.connect("alreadyarchived.db")
+cur = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS submissions(permalink TEXT)")
+
+archive_submitter = mechanize.Browser()
+
+client = praw.Reddit(user_agent="/u/PopcornArchiver Bot by /u/SirNeon")
+
+USERNAME = str(config.login.username)
+PASSWORD = str(config.login.password)
+SUBMISSION_LIMIT = int(config.main.max_submissions)
+SUBREDDIT_LIST = list(set(str(config.main.subreddit_list).split(',')))
 
 
-class PopcornArchiver(object):
+def login(username, password):
+    print("Logging in as {}...".format(username))
 
-    def __init__(self):
-        self.config = simpleconfigparser()
-
-        if not(isfile("settings.cfg")):
-            raise SettingsError("Could not find settings.cfg.")
-        else:
-            self.config.read("settings.cfg")
-
-        self.subreddit_list = list(set(self.config.main.subreddits.split(',')))
-
-        # I figured a multireddit would be the fastest way to get new posts from
-        # multiple subreddits since they can be scraped together instead of 1 by 1
-        self.multireddit = ""
-        for subreddit in self.subreddit_list:
-            self.multireddit += subreddit + '+'
-
-        self.multireddit = self.multireddit.strip('+')
-
-        self.password = self.config.login.password
-        self.verbose = self.config.main.getboolean("verbose")
-        self.username = self.config.login.username
-
-
-    def addMsg(self, msg=None, endchar='\n'):
-        """
-        Adds terminal output. Takes two optional arguments. msg defaults to None.
-        msg is the message to be printed. endchar defaults to \n. endchar is the
-        trailing character of the message to be printed.
-        """
-        if(self.verbose):
-            if msg is not None:
-                print(msg, end=endchar)
-
-            
-    def login(self):
-        """
-        This function creates a PRAW instance and logs into reddit. It uses the
-        username and password specified in the config file. Returns True if successful
-        and returns False if it could not log into reddit.
-        """
-        self.client = praw.Reddit(user_agent="Drama archiving bot by /u/SirNeon")
-        self.addMsg("Logging in as {}...".format(self.username))
-        success = True
-        
-        while True:
-            try:
-                self.client.login(self.username, self.password)
-                break
-            except InvalidUserName:
-                print("Invalid username.")
-                success = False
-                break
-            except InvalidUserPass:
-                print("Invalid password.")
-                success = False
-                break
-            except (error, HTTPError, timeout) as e:
-                self.addMsg(e)
-                continue
-            except Exception as e:
-                print(e)
-                success = False
-                break
-
-        return success
-
-            
-    def getNewSubmissions(self):
-        """
-        This function grabs submissions from a multireddit with the specified
-        subreddits in the settings.cfg file.
-        """
-
-        while True:
-            try:
-                submissions = self.client.get_subreddit(self.multireddit).get_new(limit=25)
-                break
-            except (error, HTTPError, timeout) as e:
-                self.addMsg(e)
-                continue
-
-        for submission in submissions:
-            __findLinks(submission)
-
-            
-    def __findLinks(self, submission):
-        """
-        Finds reddit links in submissions. Takes one parameter: the submission object.
-        Returns a list of links if the submission is a self-post. Returns a link if the
-        submission is a a link-post. Returns None if it doesn't find any reddit links or
-        if an AttributeError occurs.
-        """
-
-        # this matches links to reddit submissions
-        reddit_urls = re.compile("https?\:\/\/[\w.]+reddit\.com\/r\/\w+\/[\w/]+")
-
+    while True:
         try:
-            self_post = submission.is_self
-        except AttributeError as e:
-            self.addMsg(e)
+            client.login(username, password)
+            break
+        except (HTTPError, timeout) as e:
+            print(e)
+            continue
+        except (InvalidUser, InvalidUserPass, RateLimitExceeded, APIException) as e:
+            print(e)
+            exit(1)
+
+
+def get_submissions(multireddit):
+    while True:
+        try:
+            submissions = client.get_subreddit(multireddit).get_new(limit=SUBMISSION_LIMIT)
+            break
+        except (HTTPError, timeout) as e:
+            continue
+
+    return submissions
+
+
+def get_reddit_urls(submission):
+    # matches reddit submissions, but not just
+    # https://www.reddit.com/r/SomeSubreddit
+    reddit_url_pattern = re.compile("https?\:\/\/[\w.]+reddit\.com\/r\/[\w\d]+\/[\w\d/]+")
+    
+    if(submission.is_self):
+        reddit_urls = reddit_url_pattern.findall(str(submission.selftext))
+        if(reddit_urls != []):
+            return reddit_urls
+        else:
+            return None
+    else:
+        reddit_url = reddit_url_pattern.match(str(submission.url))
+        if reddit_url is not None:
+            return [reddit_url.group(0)]
+        else:
             return None
 
-        if(self_post):
-            try:
-                submission_text = str(submission.selftext)
-            except AttributeError as e:
-                self.addMsg(e)
-                return None
 
-            links = reddit_urls.findall(submission_text)
+def get_archive(link):
+    while True:
+        try:
+            archive_submitter.open("http://archive.today")
+            archive_submitter.select_form(nr=0)
+            archive_submitter["url"] = link
+            archive_submitter.submit()
+            
+            # this is necessary to get the actual archive url instead of
+            # http://archive.today/submit
+            response = archive_submitter.reload()
+            break
+        except (Exception):
+            continue
 
-            if links != []:
-                links = __removeNoParticipation(links)
-                return links
-
-            else:
-                return None
-
-        else:
-            try:
-                submission_url = str(submission.url)
-            except AttributeError as e:
-                self.addMsg(e)
-                return None
-
-            if reddit_urls.match(submission_url) is not None:
-                link = __removeNoParticipation(submission_url)
-                return link
-
-            else:
-                return None
+    return response.geturl()
 
 
-    def __removeNoParticipation(self, link=None, links=None):
-        """
-        Turns NP links into regular www links. Takes a parameter: either a string
-        with a single link or a list with multiple links in it. Returns either a
-        string with the link or a list with the new links.
-        """
+def format_comment(source_archive_dict):
+    comment_body = ""
 
-        if links is not None:
-            if type(links) is list:
-                for link in links:
-                    # so that the archives will be listed in order of appearance later
-                    position = links.index(link)
-
-                    if "np.reddit.com" in link:
-                        links.remove(link)
-                        link = link.replace("np.reddit.com", "www.reddit.com")
-                        links.insert(position, link)
-
-                    if "www.np.reddit.com" in link:
-                        links.remove(link)
-                        link = link.replace("www.np.reddit.com")
-                        links.insert(position, link)
-
-                return links
-
-            else:
-                raise TypeError("Parameter \"links\" must be a list.")
-
-        if link is not None:
-            if type(link) is str:
-                if "np.reddit.com" in link:
-                    link = link.replace("np.reddit.com", "www.reddit.com")
-
-                if "www.np.reddit.com" in link:
-                    link = link.replace("www.np.reddit.com", "www.reddit.com")
-
-                return link
-
-            else:
-                raise TypeError("Parameter \"link\" must be a string.")
-
-        
-    def getArchiveToday(self, links):
-        """
-        """
-
-        
-    def getRedditlog(self, links):
-        """
-        """
-        
-
-    def getWebArchive(self, links):
-        """
-        """
-
-
-    def formatComment(self, links):
-        """
-        """
-
-
-    def submitComment(self, text):
-        """
-        """
-
-
-    def __addToDatabase(self, link):
-        """
-        """
+    for source, archive in source_archive_dict.iteritems():
+        comment_body += "[[source]]({}) ".format(source)
+        comment_body += "[[archive]]({})\n\n".format(archive)
     
+    comment_body += "\n---\n[Open Source]({}) |".format("https://github.com/SirNeon618/PopcornArchiver")
+    comment_body += " [Feedback]({})\n".format("https://www.reddit.com/message/compose?to=%2Fr%2FPopcornArchiver")
+
+    return comment_body
+
+
+def submit_comment(submission, text):
+    while True:
+        try:
+            submission.add_comment(text)
+            break
+        except (HTTPError, timeout) as e:
+            continue
+
 
 def main():
-    Archiver = PopcornArchiver()
+    login(USERNAME, PASSWORD)
+    multireddit = ""
 
-    
-if __name__ == '__main__':
+    for subreddit in SUBREDDIT_LIST:
+        multireddit += subreddit + '+'
+
+    multireddit = multireddit.strip('+')
+
+    while True:
+        submissions = get_submissions(multireddit)
+
+        for submission in submissions:
+            permalink = str(submission.permalink)
+            cur.execute("SELECT permalink FROM submissions WHERE permalink=?", (permalink,))
+            
+            if(cur.fetchone() != None):
+                continue
+
+            archived_links = {}
+            links = get_reddit_urls(submission)
+
+            if links is not None:
+                for link in links:
+                    archived_links[link] = get_archive(link)
+                    # archive.today might get pissy with too many requests too frequently
+                    sleep(2)
+
+                text = format_comment(archived_links)
+                submit_comment(submission, text)
+
+            cur.execute("INSERT INTO submissions VALUES(?)", (permalink,))
+            con.commit()
+
+            sleep(60)
+
+
+if __name__ == "__main__":
     main()
